14c41789b59ae193833b559a272136ba41ab4115
reneboygarcia <reneboygarcia@gmail.com>
2026-06-03 15:34:47 +0800

    chore: update root formula reference template to v0.1.4

--- webp-converter.rb
+++ webp-converter.rb
@@ -3,8 +3,8 @@ class WebpConverter < Formula
 
   desc "A simple CLI tool to convert images to WebP format"
   homepage "https://github.com/reneboygarcia/webp-converter"
-  url "https://github.com/reneboygarcia/webp-converter/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "368fd59ccf3f2dbf0a77891091ecbc8ca17767b5185bfd2c771e8b4cd6c6f19f"
+  url "https://github.com/reneboygarcia/webp-converter/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "de10e53e5f3139a0a80accf1dfd2b18c364ebee984d8559f1562d51e0a0d6f9a"
   license "MIT"
 
   depends_on "python@3.12"
