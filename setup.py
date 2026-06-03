7c7192516d5796630070972a0fa699bade6c0e1b
reneboygarcia <reneboygarcia@gmail.com>
2026-06-03 15:59:06 +0800

    Bump version to 0.1.7 and implement robust KeyboardInterrupt and prompt cancellation UX/tests

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='webp-converter',
-    version='0.1.6',
+    version='0.1.7',
     description='A simple CLI tool to convert images to WebP format.',
     author='eboygarcia',
     packages=find_packages(),
