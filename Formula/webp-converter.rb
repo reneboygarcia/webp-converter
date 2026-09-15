b1e23aa8b062b0ae02dcf9b923d94a101970b368
reneboygarcia <reneboygarcia@gmail.com>
2026-09-15 13:17:14 +0800

    update Homebrew formula to v0.2.9

--- Formula/webp-converter.rb
+++ Formula/webp-converter.rb
@@ -1,8 +1,8 @@
 class WebpConverter < Formula
   desc "Fast CLI tool to convert images to WebP — Rust rewrite"
   homepage "https://github.com/reneboygarcia/webp-converter"
-  url "https://github.com/reneboygarcia/webp-converter/archive/refs/tags/v0.2.8.tar.gz"
-  sha256 "178e6acb921ba0d7e6a405fc7bfcd590a173d5cc38265443f7851c2a5bc60f76"
+  url "https://github.com/reneboygarcia/webp-converter/archive/refs/tags/v0.2.9.tar.gz"
+  sha256 "5ef4a7b718db025f2187de09bd0d9b594db6324e838c2036bf11e2826ade5745"
   license "MIT"
   head "https://github.com/reneboygarcia/webp-converter.git", branch: "main"
 
